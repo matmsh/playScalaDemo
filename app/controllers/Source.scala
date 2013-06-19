6f4099a5744973dd5bfa7ed3f31411c707e9b586
matmsh <matmsh@yahoo.com>
2013-06-19 13:01:56 +0100

    Refactored how to display source files. Now a list is passed to the templates.

--- app/controllers/Source.scala
+++ app/controllers/Source.scala
@@ -4,41 +4,52 @@ import play.api._
 import play.api.mvc._
 import play.api.Play.current
 import play.api.Play.current
+import scala.collection.mutable.ListBuffer
 
 object Source extends Controller {
 
-  case class Parameters(title:String,routes:String, messages:String,
-                 controllerSrc:String,controllerFilename:String,
-                 viewSrc:String, viewFilename:String,
-                 modelSrc:String,modelFilename:String)
+  
+  case class Display(title:String, content:String, brush:String)
+//  case class Parameters(title:String,routes:String, messages:String,
+//                 controllerSrc:String,controllerFilename:String,
+//                 viewSrc:String, viewFilename:String,
+//                 modelSrc:String,modelFilename:String)
   
   /**
    *  key should be the short name of a controller object.  
    */
   def show(key: String) = Action {
-
+    val displays = ListBuffer[Display]()
+    
     val title = current.configuration.getString(key + ".title").getOrElse("Not found")
-      
+          
+    val filter = (x:String) => (x.contains(key))    
+    val routes =  getFileAsStr("routes", filter) //getRoutesAsStr(key)
+    displays += Display("Routes",routes,"plain")
+    
+    val messages =getFileAsStr("messages", filter)
+    if (messages.nonEmpty) displays +=Display("Messages", messages,"plain")
+    
+    
     val controllerFilename = current.configuration.getString(key + ".controller").get
-    val controllerSrc = getFileAsStr(controllerFilename)
-
+    val controllerSrc = getFileAsStr(controllerFilename)    
+    displays += Display("Controller : " + controllerFilename, controllerSrc, "scala")
+    
     val viewFilename = current.configuration.getString(key + ".view").get
     val viewSrc = getFileAsStr(viewFilename)
+    displays += Display("View : " + viewFilename, viewSrc, "xml")
     
   
     val modelOption = current.configuration.getString(key + ".model")
     val modelSrc  = modelOption.map( getFileAsStr(_)).getOrElse("")
-    val modelFilename = modelOption.getOrElse("");  
+    modelOption.foreach( m => displays += Display("Model:" + m , modelSrc, "scala"))
+   
     
-    val filter = (x:String) => (x.contains(key))
-    
-    val routes =  getFileAsStr("routes", filter) //getRoutesAsStr(key)
-    val messages =getFileAsStr("messages", filter)
-    
+
      
-    val params = Parameters(title,routes,messages,controllerSrc,controllerFilename,
-                     viewSrc, viewFilename,modelSrc,modelFilename)
-     Ok(views.html.source(params))
+    //val params = Parameters(title,routes,messages,controllerSrc,controllerFilename,
+      //               viewSrc, viewFilename,modelSrc,modelFilename)
+     Ok(views.html.source(title, displays.toList))
   } 
 
 
