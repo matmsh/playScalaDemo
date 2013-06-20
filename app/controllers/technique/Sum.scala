76dcb9dea999212b6f52c410f68542018644fa82
matmsh <matmsh@yahoo.com>
2013-06-20 18:04:23 +0100

    Refactored to retain existing values on change of no of entries.

--- app/controllers/technique/Sum.scala
+++ app/controllers/technique/Sum.scala
@@ -10,24 +10,24 @@ import play.api.templates.Html
 
 object Sum extends Controller {
 
-  case class InputBean(noOfEntries:Int,entries: List[Int])
+  case class InputBean(noOfEntries: Int, entries: List[Int])
 
   lazy val inputForm = Form(
     mapping(
-        "noOfEntries" -> number,
-        "entries" -> list(number.verifying("Please enter an integer between -10 and 10 inclusively.", 
-        		        (x:Int) =>  x >= -10 && x <=10))
-        
-    )(InputBean.apply)(InputBean.unapply))
+      "noOfEntries" -> number,
+      "entries" -> list(number.verifying("Please enter an integer between -10 and 10 inclusively.",
+        (x: Int) => x >= -10 && x <= 10)))(InputBean.apply)(InputBean.unapply))
 
-    inputForm("noOfEntries")
-    
+  inputForm("noOfEntries")
+
   def show() = Action {
     implicit request =>
-      val form = if (flash.get("sum").isDefined || flash.get("change").isDefined )
+      val form = if (flash.get("sum").isDefined)
         this.inputForm.bind(flash.data)
-      else {        
-        this.inputForm.fill(InputBean(5,List()))
+      else if (flash.get("change").isDefined)
+        this.inputForm.bind(flash.data).discardingErrors
+      else {
+        this.inputForm.fill(InputBean(5, List()))
 
       }
       Ok(views.html.technique.sum(form))
@@ -35,7 +35,6 @@ object Sum extends Controller {
   }
 
   def process = Action { implicit request =>
-
     val actionOpt = request.body.asFormUrlEncoded.get("action").headOption
     Logger.info("Action=" + actionOpt)
 
@@ -48,7 +47,7 @@ object Sum extends Controller {
   }
 
   /**
-   * 
+   *
    * If no error : Compute the sum of submitted entries.
    * If has error : Display error.
    */
@@ -57,9 +56,9 @@ object Sum extends Controller {
     // formInput contains the submitted values.
     val formInput: Form[InputBean] = this.inputForm.bindFromRequest
     formInput.fold(
-      hasErrors = { formWithError =>                   
-                    Ok(views.html.technique.sum(formWithError)) 
-                },
+      hasErrors = { formWithError =>
+        Ok(views.html.technique.sum(formWithError))
+      },
       success = { inputBean =>
         val newForm = inputForm.fill(inputBean);
         // Put submitted value and the sum of entries into flash scope.
@@ -68,23 +67,39 @@ object Sum extends Controller {
       })
 
   }
-  
+
   /**
    * Redisplay the form with the newly selected no of entries.
+   * The old entries are retained.
    */
   private def change(implicit request: Request[Any]) = {
-     // formInput contains the submitted values.
-      val formInput: Form[InputBean] = this.inputForm.bindFromRequest
-        val newForm = formInput.discardingErrors
-        Logger.info("change newForm=" + newForm)
-        val noOfEntries = newForm.data("noOfEntries").toInt
-        Logger.info("change noOfEntries=" + noOfEntries)
-        
-        val newForm2 = newForm.fill(InputBean(noOfEntries,List()))
-        
-        val flashDataMap = Flash(newForm2.data) + ("change" -> "")
-        Redirect(routes.Sum.show).flashing(flashDataMap)
-      
+    // formInput contains the submitted values.
+    val formInput: Form[InputBean] = this.inputForm.bindFromRequest
+
+    val noOfEntries = formInput.data("noOfEntries").toInt
+    val entryFilter = getFilter(noOfEntries)
+    val dataMapNew = formInput.data.filter(x => entryFilter(x._1))
+
+    val flashDataMap = Flash(dataMapNew) + ("change" -> "")
+    Redirect(routes.Sum.show).flashing(flashDataMap)
+
+  }
+
+  /**
+   *  Return a filter that will retains keys like entries[i] where i <= noOfEntries and
+   *  key does not starts with "entries".
+   */
+  private def getFilter(noOfEntries: Int) = {
+    val retainEntries = (0 to noOfEntries - 1).map(i => "entries[" + i + "]")
+    (key: String) => !key.startsWith("entries") || retainEntries.contains(key)
+
   }
 
 }
+
+
+
+
+
+
+
